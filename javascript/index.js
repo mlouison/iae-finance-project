14ef7e349741d55ad30f48c6c150d0d06370b9c9
Medhi Louison <45641368+mlouison@users.noreply.github.com>
2023-10-30 00:56:18 +0100

    page FAQ + font

--- javascript/index.js
+++ javascript/index.js
@@ -14,6 +14,33 @@ document.addEventListener("DOMContentLoaded", function() {
     let navQui = document.getElementById("navQui");
     let navContact = document.getElementById("navContact");
     let navFaq = document.getElementById("navFaq");
+    let toggles = document.querySelectorAll(".toggle");
+    let contentDiv = document.querySelectorAll(".content");
+    let icons = document.querySelectorAll(".icon");
+    for(let i = 0; i < toggles.length; i++){
+        toggles[i].addEventListener("click", function(){
+            if(parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight){
+                contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+                toggles[i].style.color ="#0084e9";
+                icons[i].classList.remove("fa-plus");
+                icons[i].classList.add("fa-minus");
+            }else{
+                contentDiv[i].style.height = "0px";
+                toggles[i].style.color ="#000";
+                icons[i].classList.remove("fa-minus");
+                icons[i].classList.add("fa-plus");
+            }
+            for(let j = 0; j < toggles.length; j++){
+                if(j != i){
+                    contentDiv[j].style.height = "0px";
+                    toggles[j].style.color ="#000";
+                    icons[j].classList.remove("fa-minus");
+                    icons[j].classList.add("fa-plus");
+                }
+            }
+        });
+        
+    }
 
     document.addEventListener("click", function(e) {
         if(e.target == accueil || e.target == navAccueil) {
